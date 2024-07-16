e0727e43875afd75427176630d186b1816a00843
Angl99 <anggelplasencia@pursuit.org>
2024-07-16 17:27:05 -0400

    initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: 'Poppin',
+        imgUrl: 'public/assets/poppin.png',
+        stack: ['Vite', 'React', 'JavaScript', 'Styled Components', 'PostgreSQL', 'Prisma'],
+        link: 'https://github.com/Angl99/poppin'
+    },
+    {
+        title: 'Developer Portfolio',
+        imgUrl: 'public/assets/portfolio.png',
+        stack: ['Vite', 'React', 'JavaScript', 'Tailwind'],
+        link: 'https://github.com/Angl99/portfolio'
+    },
+    {
+        title: 'Disney+ Clone',
+        imgUrl: 'public/assets/disneyClone.png',
+        stack: ['Vite', 'React', 'JavaScript', 'Redux', 'Styled Components', 'Firebase'],
+        link: 'https://github.com/Angl99/DisneyPlus-Clone'
+    },
+    {
+        title: 'Youtube Clone',
+        imgUrl: 'public/assets/YoutubeClone.png',
+        stack: ['React', 'JavaScript', 'HTML','CSS'],
+        link: 'https://github.com/Angl99/03-03p-youtube-clone'
+    },
+    {
+        title: 'AniTrack',
+        imgUrl: 'public/assets/AniTrack.png',
+        stack: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/Angl99/Front-end-Portfolio-Project'
+    },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: '2024',
+        title: 'Participated in Pursuit Hackathon sponsored by the New York Times!',
+        duration: '3 Days',
+        details: 'Participated in Pursuit Hackathon sponsored by the New York Times, with the goal of using the NYT Api to make a way for people to interact with the news in a more interesting way.'
+    },
+    {
+        year: '2023',
+        title: 'Joined the Pursuit Program',
+        duration: '10 Months',
+        details:'Joined the pursuit program to move myself to the next level of my career and further my passion for software engineering.'
+    },
+    {
+        year: '2022',
+        title: "Bachelor's Degree in Information Technology",
+        duration: '4 years',
+        details:'Started my career in Tech and introduced me to the world of software through some computer science classes.'
+    },
+]
